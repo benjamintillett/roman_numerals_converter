aa21629e9e6e1b827238e6e1a405a8bccb1f0899
benjamintillett <benjamin.tillett@gmail.com>
2014-08-15 08:10:04 +0100

    add test for 1 returning 'I'

---
+++ lib/roman_numerals.rb
@@ -0,0 +1,3 @@
+def roman_numerals(number)
+	"I"
+end
---
+++ spec/roman_numerals_spec.rb
@@ -0,0 +1,9 @@
+require 'roman_numerals'
+
+describe "roman_numerals" do
+	it "given 1 should return 'I'" do 
+		expect(roman_numerals(1)).to eq "I"
+	end 
+
+
+end 
