97f29f96f0bcd8c57100960d8833065e03aed7df
benjamintillett <benjamin.tillett@gmail.com>
2014-08-15 08:31:37 +0100

    add specs: given 10 it should return 'X', given 14 it should return 'XIV',given 19 it should return 'XIX'

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,3 +1,3 @@
 def roman_numerals(number)
-	("I"*number).gsub("IIIII","V").gsub("IIII","IV")
+	("I"*number).gsub("I"*10,"X").gsub("I"*9,"IX").gsub("IIIII","V").gsub("IIII","IV")
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -16,4 +16,13 @@ describe "roman_numerals" do
 	it "given 6 should return 'VI'" do
 		expect(roman_numerals(6)).to eq "VI"
 	end
+	it "given 10 it should return 'X'" do
+		expect(roman_numerals(10)).to eq "X"
+	end
+	it "given 14 it should return 'XIV'" do
+		expect(roman_numerals(14)).to eq "XIV"
+	end
+	it "given 19 it should return 'XIX'" do
+		expect(roman_numerals(19)).to eq "XIX"
+	end
 end 
