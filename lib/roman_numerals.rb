8c51104e695077aca556dde29805afe71d19b141
benjamintillett <benjamin.tillett@gmail.com>
2014-08-15 08:45:29 +0100

    working roman numerals whey

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,3 +1,3 @@
 def roman_numerals(number)
-	("I"*number).gsub("I"*10,"X").gsub("I"*9,"IX").gsub("IIIII","V").gsub("IIII","IV")
+	("I"*number).gsub("I"*1000,"M").gsub("I"*900,"CM").gsub("I"*500,"D").gsub("I"*400,"CD").gsub("I"*100,"C").gsub("I"*90,"XC").gsub("I"*50,"L").gsub("I"*40,"LX").gsub("I"*10,"X").gsub("I"*9,"IX").gsub("IIIII","V").gsub("IIII","IV")
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -25,4 +25,45 @@ describe "roman_numerals" do
 	it "given 19 it should return 'XIX'" do
 		expect(roman_numerals(19)).to eq "XIX"
 	end
+	it "given 26 it should return 'XXVI'" do
+		expect(roman_numerals(26)).to eq "XXVI"
+	end
+	it "given 40 it should return 'LX'" do
+		expect(roman_numerals(40)).to eq "LX"
+	end
+	it "given 50 it should return '50'" do
+		expect(roman_numerals(50)).to eq "L"
+	end	
+	it "given 90 should return XC" do 
+		expect(roman_numerals(90)).to eq "XC"
+	end
+	it "given 100 should return C" do 
+		expect(roman_numerals(100)).to eq "C"
+	end
+	it "given 151 should return CLI" do 
+		expect(roman_numerals(151)).to eq "CLI"
+	end
+	it "given 400 should return CD" do 
+		expect(roman_numerals(400)).to eq "CD"
+	end
+	it "given 500 should return D" do 
+		expect(roman_numerals(500)).to eq "D"
+	end
+	it "given 900 should return CM" do 
+		expect(roman_numerals(900)).to eq "CM"
+	end
+	it "given 1000 should return M" do 
+		expect(roman_numerals(1000)).to eq "M"
+	end
+	it "given 1000 should return M" do 
+		expect(roman_numerals(1000)).to eq "M"
+	end	
+	it "given 4569 should return MMMMDLXIX" do 
+		expect(roman_numerals(4569)).to eq "MMMMDLXIX"
+	end	
+
+
+
+
 end 
+
